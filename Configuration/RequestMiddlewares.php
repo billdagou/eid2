9081e30ddacd9b3e041ba445cd4330fddf7d7097
Bill.Dagou <billdagou@gmail.com>
2022-01-14 22:22:09 +0800

    Fix DI

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'frontend' => [
-        'dagou/eid' => [
+        'dagou/eid2' => [
             'target' => \Dagou\Eid2\Middleware\Eid2Handler::class,
             'after' => [
                 'typo3/cms-frontend/prepare-tsfe-rendering',
